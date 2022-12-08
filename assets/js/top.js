6e01c0cf4c77798a56c28b8df774825ff15d6574
tomiyakyain <tomiyakyain@gmail.com>
2022-12-08 15:23:58 +0900

    fixed excess margin right bug

--- assets/js/top.js
+++ assets/js/top.js
@@ -1,6 +1,5 @@
 var heroSwiper = new Swiper(".heroSwiper", {
   direction: "vertical",
-  spaceBetween: 50,
   effect: "fade",
   pagination: {
     el: ".swiper-pagination",
@@ -9,10 +8,9 @@ var heroSwiper = new Swiper(".heroSwiper", {
 });
 
 var playSwiper = new Swiper(".playSwiper", {
-        slidesPerView: 3,
-        spaceBetween: 30,
+        slidesPerView: 'auto',
+        spaceBetween: 20,
         initialSlide: 2,
-        calculateHeight:true,
         navigation: {
           nextEl: ".swiper-button-next",
           prevEl: ".swiper-button-prev",
