326b541071e90982e96b4b33528ecfa878e3d158
tomiyakyain <tomiyakyain@gmail.com>
2022-12-13 11:25:03 +0900

    centered between slides

--- assets/js/top.js
+++ assets/js/top.js
@@ -15,7 +15,7 @@ var playSwiper = new Swiper(".playSwiper", {
         slidesPerView: 'auto',
         centeredSlides:true,
         spaceBetween: 20,
-        initialSlide: 2,
+        initialSlide: 1,
         navigation: {
           nextEl: ".swiper-button-next",
           prevEl: ".swiper-button-prev",
